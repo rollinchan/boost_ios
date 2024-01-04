a10bcd5362a9fe108af41e9f8336d33562a41c50
RonaldC <hchan8355@gmail.com>
2024-01-04 17:27:56 +0800

    no message

--- one_app/AppDelegate.swift
+++ one_app/AppDelegate.swift
@@ -6,29 +6,33 @@
 //
 
 import UIKit
+import flutter_boost
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
+    var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let vc = MainViewController()
+        var nav = UINavigationController(rootViewController: vc)
+    
+        
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+        
+        //创建代理，做初始化操作
+        let delegate = BoostDelegate.shared
+        FlutterBoost.instance().setup(application, delegate: delegate) { engine in
+
+            
+        }
+        
         // Override point for customization after application launch.
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
 
--- one_app/ViewController.swift
+++ one_app/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import flutter_boost
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let btn = UIButton()
+        btn.setTitle("title", for: .normal)
+        btn.addTarget(self, action:#selector(btnClick(_:)), for: .touchUpInside)
+        self.view.backgroundColor = .red
+        btn.frame = CGRect(x: 10, y: 100, width: 60, height: 40)
+        self.view.addSubview(btn)
+        
+        let newLab = UIButton()
+        newLab.setTitle("title2", for: .normal)
+        newLab.addTarget(self, action:#selector(btn2Click(btn:)), for: .touchUpInside)
+        self.view.backgroundColor = .red
+        newLab.frame = CGRect(x: 80, y: 100, width: 60, height: 40)
+        self.view.addSubview(newLab)
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @objc func btnClick(_ sender:UIButton){
+        self.navigationController?.isNavigationBarHidden = true
+        print("btnClick")
+        BoostDelegate.shared.navigationController = self.navigationController
+        
+        let options = FlutterBoostRouteOptions()
+        options.pageName = "flutterPage"
+        options.onPageFinished = { dic in
+            print(" dic ----- \(String(describing: dic?["retval"]))")
+        }
+        FlutterBoost.instance().open(options)
+        
+        print("end -   --  - btnClick")
+    }
+    
+    @objc func btn2Click(btn sender:UIButton){
+        self.navigationController?.isNavigationBarHidden = true
+        print("btnClick")
+        BoostDelegate.shared.navigationController = self.navigationController
+        
+        let options = FlutterBoostRouteOptions()
+        options.pageName = "newsPage"
+        options.onPageFinished = { dic in
+            print(" dic ----- \(String(describing: dic?["retval"]))")
+        }
+        FlutterBoost.instance().open(options)
+        
+        print("end -   --  - btnClick")
+    }
 }
 
