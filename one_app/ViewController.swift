c05863fc6fb760929056982553e68581a8de3552
RonaldC <hchan8355@gmail.com>
2024-01-04 14:04:24 +0800

    Initial Commit

---
+++ one_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  one_app
+//
+//  Created by smith on 2024/1/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
